fb5eab99f6cac6aed6c47e77b1e32af8c4dcf936
Sam <samhooker@gmail.com>
2024-11-19 10:52:48 -0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import * as d3 from 'd3';
+	import LinePlot from '../components/LinePlot.svelte';
+	let data = d3.ticks(-2, 2, 200).map(Math.sin);
+
+	function onMousemove(e) {
+		const [x, y] = d3.pointer(e);
+		data = data.slice(-200).concat(Math.atan2(x, y));
+	}
+</script>
+
+<div onmousemove={onMousemove}>
+	<LinePlot {data} />
+</div>
